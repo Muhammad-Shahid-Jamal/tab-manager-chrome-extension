7d5d42955be834ceeaadaee535d2efae2ce08f94
Shahid <shahid.jamal0343@gmail.com>
2023-03-27 00:22:49 +0500

    feat: tab list update and tab focusing and tab closing
    when user click on tab detail it will navigate to the tab
    when user click close icon tab will be closed

--- popup/js/popup.js
+++ popup/js/popup.js
@@ -9,16 +9,21 @@ const template = document.getElementById('li_template_for_tabs');
 const elements = new Set();
 
 for (const tab of tabs) {
+  console.log(tab);
   const element = template.content.firstElementChild.cloneNode(true);
   const { title, favIconUrl, url } = tab;
   if (url && url.indexOf('127.0.0.1') === -1) {
     const pathName = new URL(url).origin;
-
     element.querySelector('.title').textContent = title;
     element.querySelector('img').src = favIconUrl;
     element.querySelector('.pathname').textContent = pathName;
     element.querySelector('a').addEventListener('click', async () => {
-      console.log(tab);
+      await chrome.tabs.update(tab.id, { active: true });
+      await chrome.windows.update(tab.windowId, { focused: true });
+    });
+    element.querySelector('button').addEventListener('click', async () => {
+      await chrome.tabs.remove(tab.id);
+      element.remove();
     });
     elements.add(element);
   }
